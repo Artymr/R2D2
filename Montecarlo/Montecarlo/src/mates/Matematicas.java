c0f876ed29029cf512e10e5b7a5483fd5b670af1
Arturo Muñoz <arturo.munozrodriguez@usp.ceu.com>
2022-03-21 23:09:13 +0100

    Lambda

--- Montecarlo/Montecarlo/src/mates/Matematicas.java
+++ Montecarlo/Montecarlo/src/mates/Matematicas.java
@@ -9,22 +9,20 @@ public class Matematicas{
 	* @param pasos que es el numero de "dardos" que se van a lanzar sobre el área para obtener la aproximación a pi
 	* @return el numero aproximado a pi
 	*/
-    public static double generarNumeroPiRecursivo(long pasos){
+    public static double generarNumeroPiLambda(long pasos){
         int i = 0;
         int count = 0;
         double pi;
         double x = 0;
         double y = 0;
-        while(i < pasos) {
-            x = Math.random();
-            y = Math.random();
-            if ((pasos == 0)
-		return 0;
-	    else 
-		return pasos* (generarNumeroPiRecursivo (pasos -1));{
-                count++;
-            }
-        }
+
+	ArrayList<Double> pasos = new ArrayList();
+	
+	for(double i = 0; i <= pasos: i++){
+		double x = Math.random();
+		double y = Math.random();
+
+    }
         pi = 4 * ((float)count / (float)pasos);
        return pi;
     }
